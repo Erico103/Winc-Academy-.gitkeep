da735343d1fae98d04fa13d16bffea420ef15519
Erico103 <85289051+Erico103@users.noreply.github.com>
2021-07-22 14:14:30 +0200

    Add files via upload

---
+++ function2.js
@@ -0,0 +1,20 @@
+function makePizza(){
+    console.log("Get a crispy pizza bottom")
+    console.log("Layer it with cheese")
+    console.log("Then layer it with tomatoes")
+    console.log("Put it in your oven")
+    console.log("walla a pizza")
+}
+
+function makeSushi(){
+    console.log("Take some fish")
+    console.log("cut it in bitesize chunks")
+    console.log("make the sojasauce")
+    console.log("dont cook it")
+    console.log("walla, some sushi")
+}
+makeSushi()
+makeSushi()
+makeSushi()
+makePizza()
+makePizza()
